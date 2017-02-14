6f26f6bf94ecf79d9ae4f6eb2d642dab40c5dd5c
Xia Amendolara <xiaesmeamen@me.com>
2017-02-14 11:30:16 -0800

    created test 1 checkCharacters, and passed

--- src/anagram.php
+++ src/anagram.php
@@ -2,9 +2,21 @@
 
     function Anagram
     {
+        function checkCharacters {
+            <?php
+            $a = "tale";
+            $b = "bin";
 
-        function __construct()
-         {
+
+            $countA = count_chars($a, 1);
+            $countB = count_chars($b, 1);
+
+        if ($countA == $countB)
+        {
+        	echo "These words match $a and $b ";
+        } else {
+        	echo "There is not a match";
+        }
 
         }
     }
